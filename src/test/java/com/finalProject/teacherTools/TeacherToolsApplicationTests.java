39fcdd98d9b3869405ca4c115c3ebe792593d23a
adamyoungpeter <adamyoungpeter@gmail.com>
2021-08-02 10:32:52 -0400

    initial commit

---
+++ src/test/java/com/finalProject/teacherTools/TeacherToolsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.finalProject.teacherTools;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TeacherToolsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
