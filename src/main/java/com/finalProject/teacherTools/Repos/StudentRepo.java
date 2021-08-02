6e2e6750f501525e7555060c66d97ca5e97280f9
Josh Hardwick <xasummersfallx@msn.com>
2021-08-02 14:45:37 -0400

    Built up all Repos for backend.

---
+++ src/main/java/com/finalProject/teacherTools/Repos/StudentRepo.java
@@ -0,0 +1,7 @@
+package com.finalProject.teacherTools.Repos;
+
+import com.finalProject.teacherTools.Models.Student;
+import org.springframework.data.repository.CrudRepository;
+
+public interface StudentRepo extends CrudRepository<Student, Long> {
+}
