ecad30399286ee9a10824f0a69f010081c7fb56f
Josh Hardwick <xasummersfallx@msn.com>
2021-08-03 10:50:40 -0400

    Built Home page Controller.

---
+++ src/main/java/com/finalProject/teacherTools/Controllers/HomePageController.java
@@ -0,0 +1,22 @@
+package com.finalProject.teacherTools.Controllers;
+
+
+import com.finalProject.teacherTools.Repos.ClassroomRepo;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomePageController {
+    private ClassroomRepo classroomRepo;
+
+    public HomePageController(ClassroomRepo classroomRepo) {
+        this.classroomRepo = classroomRepo;
+    }
+
+    @GetMapping("/")
+    public String displayHomePage(Model model) {
+        model.addAttribute("class rooms", classroomRepo.findAll());
+        return "home-page-template";
+    }
+}
