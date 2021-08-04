0d2a5d1bfcf87bf4f7489d51a220514dde9c4d60
Grant Cushman <cushman.grant@gmail.com>
2021-08-04 11:27:33 -0400

    set up single and all students html, created AssignmentController

---
+++ src/main/java/com/finalProject/teacherTools/Controllers/AssignmentController.java
@@ -0,0 +1,25 @@
+package com.finalProject.teacherTools.Controllers;
+
+import com.finalProject.teacherTools.Models.Assignment;
+import com.finalProject.teacherTools.Repos.AssignmentRepo;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class AssignmentController {
+    private AssignmentRepo assignmentRepo;
+
+    public AssignmentController(AssignmentRepo assignmentRepo) {
+        this.assignmentRepo = assignmentRepo;
+    }
+
+    @GetMapping("/assignment")
+    public String displayAssignmentPage(@RequestParam("id") Long id, Model model){
+        Assignment assignment = assignmentRepo.findById(id).get();
+        model.addAttribute("individualAssignment", assignment);
+        return "single-assignment-template";
+    }
+
+}
