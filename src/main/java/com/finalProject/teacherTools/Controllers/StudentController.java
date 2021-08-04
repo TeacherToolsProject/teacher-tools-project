766b703c3aa767da897951e35348328305ced80c
Grant Cushman <cushman.grant@gmail.com>
2021-08-04 10:44:14 -0400

    new StudentController, htmls for single-student and all students. linked to those in home-page

--- src/main/java/com/finalProject/teacherTools/Controllers/StudentController.java
+++ src/main/java/com/finalProject/teacherTools/Controllers/StudentController.java
@@ -1,8 +1,32 @@
 package com.finalProject.teacherTools.Controllers;
 
+import com.finalProject.teacherTools.Models.Student;
+import com.finalProject.teacherTools.Repos.StudentRepo;
+import org.dom4j.rule.Mode;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class StudentController {
-    
+    private StudentRepo studentRepo;
+
+    public StudentController(StudentRepo studentRepo) {
+        this.studentRepo = studentRepo;
+    }
+
+    @GetMapping("/students")
+    public String displayAllStudents(Model model){
+        Iterable<Student> students = studentRepo.findAll();
+        model.addAttribute("students", students);
+        return "students-template";
+    }
+
+    @GetMapping("/student")
+    public String displayStudentPage(@RequestParam("id") Long id, Model model) {
+        Student student = studentRepo.findById(id).get();
+        model.addAttribute("individualStudent", student);
+        return "single-student-template";
+    }
 }
